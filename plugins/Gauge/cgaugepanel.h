2bc6a6303c25ef6560cbfef5575e879c95c8eea6
Vit Stancl <stancl@3dim-laboratory.cz>
2014-12-19 11:34:03 +0100

    (3DV) Missing files.

---
+++ plugins/Gauge/cgaugepanel.h
@@ -0,0 +1,37 @@
+///////////////////////////////////////////////////////////////////////////////
+// $Id$
+//
+
+#ifndef CGAUGEPANEL_H
+#define CGAUGEPANEL_H
+
+#include <QWidget>
+
+#include <qtplugin/PluginInterface.h>
+
+namespace Ui {
+class CGaugePanel;
+}
+
+class CGaugePanel : public QWidget, public CAppBindings
+{
+    Q_OBJECT
+    
+public:
+    explicit CGaugePanel(CAppBindings* pBindings, QWidget *parent = 0);
+    ~CGaugePanel();
+
+    //! set last read density to panel
+    void setDensity(int nValue);
+
+    //! set last read distance to panel
+    void setDistance(double fValue);
+    
+private slots:
+    void on_comboBoxMeasuringMode_currentIndexChanged(int index);
+
+private:
+    Ui::CGaugePanel *ui;
+};
+
+#endif // CGAUGEPANEL_H
